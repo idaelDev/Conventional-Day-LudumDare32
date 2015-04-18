0652862313a33d4513baf15e67502a84ec76eff3
Vincent Swaenepoel <swa.vincent@gmail.com>
2015-04-18 10:54:32 +0200

    enemy1

---
+++ Assets/Scripts/Enemy1Manager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy1Manager : MonoBehaviour {
+
+    public float speed = 2;
+    public float gravity = 10;
+
+    private bool goToLeft = true;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector2 direction = Vector2.zero;
+        if (goToLeft)
+        {
+            direction.x = -1;
+        }
+        else
+            direction.x = 1;
+
+        direction.x *= speed;
+
+        transform.Translate(direction);
+	}
+}
