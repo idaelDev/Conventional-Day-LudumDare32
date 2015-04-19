56988459e109ffec68e18676902850d34c5e2310
idaelDev <judicael.abecassis@gmail.com>
2015-04-19 13:25:17 +0200

    End scene

---
+++ Assets/TextFader.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TextFader : MonoBehaviour {
+
+    public Text text;
+    public float smooth = 1f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        text.color = Color.Lerp(text.color, new Color(1,1,1,1), smooth);
+	}
+}
