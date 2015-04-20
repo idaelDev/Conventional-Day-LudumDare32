eff9f2b75bf8fa0ac626e2cd1bb71eb287c7db91
Vincent Swaenepoel <swa.vincent@gmail.com>
2015-04-20 13:59:43 +0200

    finMenu + crédit + début pause

---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Pause : MonoBehaviour {
+
+	private bool pause = false;
+	private Canvas cvsPause;
+
+	// Use this for initialization
+	void Start () {
+		cvsPause = GetComponent<Canvas>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown ("Submit")) {
+			pause = !pause;
+		}
+
+		if (pause) {
+			cvsPause.enabled = true;
+			Time.timeScale = 0;
+		} else {
+			cvsPause.enabled = false;
+			Time.timeScale = 1;
+		}
+		
+	}
+
+	public void QuitToMenu(){
+		Application.LoadLevel(0);
+	}
+
+	public void Restart(){
+		Application.LoadLevel (Application.loadedLevel);
+	}
+
+	public void ReturnGame(){
+		pause = false;
+	}
+}
--- Assets/Scripts/TextFader.cs
+++ Assets/Scripts/TextFader.cs
@@ -5,15 +5,31 @@ using UnityEngine.UI;
 public class TextFader : MonoBehaviour {
 
     public Text text;
+	public float wait = 0f;
     public float smooth = 1f;
+	public bool pressToQuit = false;
+
+	private bool activeFade = false;
 
 	// Use this for initialization
 	void Start () {
-	
+		StartCoroutine (StartFade ());
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        text.color = Color.Lerp(text.color, new Color(1,1,1,1), smooth);
+		if (activeFade) {
+
+			text.color = Color.Lerp (text.color, new Color (1, 1, 1, 1), smooth);
+
+			if(pressToQuit && Input.anyKeyDown){
+				Application.Quit();
+			}
+		}
+	}
+
+	IEnumerator StartFade(){
+		yield return new WaitForSeconds(wait);
+		activeFade = true;
 	}
 }
